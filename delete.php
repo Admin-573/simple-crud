0f8fdce084a4445317a472567eb07e4ff4da24b3
kevat <kevatrajat29@gmail.com>
2023-09-19 14:07:14 +0530

    normalcrud

--- delete.php
+++ delete.php
@@ -2,21 +2,6 @@
     include "crud_init.php";
     $delID=$_GET['id'];
     $queryExe = "DELETE FROM $crudtbl WHERE $delID=$id";
-    $sql = mysqli_query($con,$queryExe);
-    if($sql)
-    {
-        ?> 
-            <script>
-                alert("Deleted");
-                window.open("http://localhost/simple%20crud/index.php","_self");
-            </script>
-        <?php
-    }
-    else {
-        ?> 
-            <script>
-                alert("Error Occured");
-            </script>
-        <?php
-    }
+    mysqli_query($con,$queryExe);
+    header("Location:index.php");
 ?>
